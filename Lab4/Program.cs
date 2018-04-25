2125ef02b0ddc8484ef2c5b4ec3a3ea7b0912f1d
Nadim <syed.m.nadim@gmail.com>
2018-04-25 09:01:01 -0400

    First Commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static int Squared(int integer)
+        {
+            int Squared = integer * integer;
+            return Squared;
+        }
+
+        static int Cubed(int integer)
+        {
+            int Cubed = Squared(integer) * integer;
+            return Cubed;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!");
+
+            do
+            {
+                Console.Write("Enter an integer: ");
+                string integer = Console.ReadLine();
+
+                bool worked = int.TryParse(integer, out int integer2);
+
+                if (worked)
+                {
+                    Console.WriteLine("Number           Squared         Cubed");
+                    Console.WriteLine("======           =======         =====");
+                    //string s = string.Format("{0,-10} {1, -10:N0}\n", Squared, Cubed);
+                    for (int i = 1; i <= integer2; i++)
+                    {
+                        //s += string.Format("{0,-10} {1, -10:N0}\n", Squared(i), Cubed(i));
+                        Console.WriteLine(i + "                " + Squared(i) + "               " + Cubed(i));
+                        //Console.WriteLine(s);
+                    }
+
+                    Console.Write("Continue? (y/n): ");
+                    var yep = Console.ReadLine();
+                    bool quitter = char.TryParse(yep, out char quitter2);
+                    if(quitter2 == 'N' || quitter2 == 'n')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter and integer! =( ");
+                }
+
+
+            } while (true);
+            
+
+        }
+    }
+}
